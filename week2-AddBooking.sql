4ec8f16c84fe319e3943e29084472863dc21bb56
tanviredu <tanviredu2018@gmail.com>
2023-09-18 13:14:38 +0600

    added

--- week2-AddBooking.sql
+++ week2-AddBooking.sql
@@ -17,22 +17,3 @@ END //
 DELIMITER ;
 
 CALL AddBooking(3,"2022-12-30",3,3);
-
-Task-2 :  UPDATE Booking
-
-USE LittleLemonDB;
-
-DROP PROCEDURE IF EXISTS UpdateBooking;
-DELIMITER //
-CREATE PROCEDURE UpdateBooking(
-    booking_id INT,
-    booking_date DATETIME
-)
-BEGIN
-    UPDATE Bookings SET BookingDate = booking_date
-    WHERE BookingID = booking_id;
-    SELECT CONCAT("Booking ",booking_id," ipdated");
-
-END //
-DELIMITER ;
-CALL UpdateBooking(1,"2022-10-20");
